4638f6ad13ce2bab498770d8bf830a060ac7c142
Alcatraz <markin201045@gmail.com>
2019-05-07 14:47:09 +0800

    optimize

--- test_code.cpp
+++ test_code.cpp
@@ -5,52 +5,60 @@
 using namespace std;
 using namespace cv;
 
-void Polygon_detection(Mat &srcImg,Mat dstImg,vector<Point2f> &out_mc){
+void Polygon_detection(Mat &srcImg,Mat &dstImg,vector<Point2f> &out_mc,vector<int> &shape){
   cvtColor(srcImg, srcImg, CV_BGR2GRAY);
   threshold(srcImg, srcImg, 200, 255, CV_THRESH_BINARY_INV);
+
   vector<vector<Point> > contours;
   vector<Vec4i> hierarcy;
   findContours(srcImg, contours, hierarcy, 0, CV_CHAIN_APPROX_NONE);
+
+
   vector<Moments>mu(contours.size());
-  vector<vector<Point> > contours_poly(contours.size()); //用于存放折线点集
+  vector<vector<Point> > contours_poly(contours.size()); 
   vector<Point2f>mc(contours.size());
+
   for (int i = 0; i < contours.size(); i++) {
     double epsilon = 0.01 * arcLength(contours[i], true);
     approxPolyDP(Mat(contours[i]), contours_poly[i], epsilon, true);
-    drawContours(dstImg, contours_poly, i, Scalar(0, 255, 255), 2, 8); //绘制
-    cout << contours_poly[i].size() << endl;
+    drawContours(dstImg, contours_poly, i, Scalar(0, 255, 255), 2, 8); 
     mu[i] = moments(contours_poly[i], false);
     mc[i] = Point2f(mu[i].m10 / mu[i].m00, mu[i].m01 / mu[i].m00);
     circle(dstImg, mc[i], 4, Scalar(255,0,0), -1, 8, 0);  
     out_mc.push_back(mc[i]);
-    switch(contours_poly[i].size()){
-      case 3:
-        cout<< "Triangle:position:("<<mc[i]<<endl;
-      break;
-      case 4:
-        cout<< "Rectangle:position:("<<mc[i]<<endl;
-      break;
-      case 5:
-        cout<< "Pentagon:position:("<<mc[i]<<endl;
-      break;
-      case 6:
-        cout<< "Hexagon:position:("<<mc[i]<<endl;
-      break;
-      default:
-        cout<< "Circle:position:("<<mc[i]<<endl;
-    }
+    shape.push_back((int)contours_poly[i].size());
   }
 
-  imshow("approx", dstImg);
-
-  waitKey(0);
 }
 
 int main() {
   Mat srcImg = imread("test_pic.png");
   Mat dstImg(srcImg.size(), CV_8UC3, Scalar::all(0)); //纯黑图像
   vector<Point2f>mc;
-  Polygon_detection(srcImg,dstImg,mc);
-  cout<<mc<<endl;
+  vector<int> shape;
+  Polygon_detection(srcImg,dstImg,mc,shape);
+
+  for (int i=0;i<shape.size();i++){
+    switch(shape[i]){
+      case 3:
+        cout<< "Triangle:position:(";
+      break;
+      case 4:
+        cout<< "Rectangle:position:(";
+      break;
+      case 5:
+        cout<< "Pentagon:position:(";
+      break;
+      case 6:
+        cout<< "Hexagon:position:(";
+      break;
+      default:
+        cout<< "Circle:position:(";
+    }
+    cout<<mc[i]<<")"<<endl;
+  }
+
+  imshow("approx", dstImg);
+  waitKey(0);
   return 0;
 }
